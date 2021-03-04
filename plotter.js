baf423b4091068a7d2c6b5dede62b45d5c7a2217
AgusNeira <a2002neira@gmail.com>
2021-03-03 21:34:08 -0300

    [ADD] Main app structure with split screen and d3.js

---
+++ plotter.js
@@ -0,0 +1,3 @@
+let svg = d3.select('#plot-space').append('svg')
+    .attr('class', 'plot')
+    .attr('viewBox', [0, 0, 600, 400]);
---
+++ split-screen.js
@@ -0,0 +1,27 @@
+window.onload = function() {
+    let splitScreen = document.querySelector('.split-screen');
+    let leftScreen = splitScreen.querySelector('.left-screen');
+    let rightScreen = splitScreen.querySelector('.right-screen');
+    let resizer = splitScreen.querySelector('#resizer');
+
+    let totalWidth = splitScreen.offsetWidth;
+    let x = 0;
+
+    resizer.onmousedown = mouseDownHandler;
+
+    function mouseDownHandler(e) {
+        x = e.clientX;
+        document.addEventListener('mousemove', mouseMoveHandler);
+        document.addEventListener('mouseup', mouseUpHandler);
+    }
+    function mouseMoveHandler(e) {
+        let dx = e.clientX - x;
+        x = e.clientX;
+
+        leftScreen.style.width = leftScreen.offsetWidth + dx;
+    }
+    function mouseUpHandler(e) {        
+        document.removeEventListener('mousemove', mouseMoveHandler);
+        document.removeEventListener('mouseup', mouseUpHandler);
+    }
+}
