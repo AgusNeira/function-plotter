9f45fb973daf03ca085911688e21e26dab8cce32
AgusNeira <a2002neira@gmail.com>
2021-06-08 19:09:08 -0300

    [ADD] Function now successfully rescales with full resolution

--- src/plotter.js
+++ src/plotter.js
@@ -1,47 +1,45 @@
 const { evaluate } = require('./../../expression-parser/index.js');
 
-console.log(evaluate);
-
-
 export function plotter() {
     /*d3.select('#equation-input')
         .on('input', e => equationParser(e.target.value));*/
 
-    let root = d3.select('#plot-space');
-    let size = { width: 600, height: 400 };  
+    let root = d3.select('#plot-space'),
+        size = root.node().getBoundingClientRect(),
+        aspectRatio = size.height / size.width,
+        resolution = 200, // points used to draw the function
+        plotRanges = [[-100, 100], [-100 * aspectRatio, 100 * aspectRatio]],
+        data = [],
+        zoomDelay = 200,
+        zoomInterval = 1.25;
 
-
-    let plotRanges = [[-100, 100], [-100, 100]];
-
-
-    let data = [];
     // Load data with linear function
-    for (let i = plotRanges[0][0]; i <= plotRanges[0][1]; i++){
-        data.push({ x: i, y: i });
+    function load() {
+        data = [];
+        let resolutionScale = d3.scaleLinear()  // used to break the current
+            .domain([0, resolution])            // into n evenly separated
+            .range(plotRanges[0]);              // points
+
+        for (let i = 0; i <= resolution; i++) {
+            data.push({
+                x: resolutionScale(i),
+                y: resolutionScale(i)**3
+            });
+        }
     }
 
     let svg = d3.select('#plot-space')
         .append('svg')
         .attr('class', 'plot')
         .attr('viewBox', [0, 0, size.width, size.height])
-        .attr('preserveAspectRatio', 'xMidYMid slice');
+        .attr('preserveAspectRatio', 'xMidYMid slice')
+        .on('resize', SVGResizeHandler)
+        .on('wheel', delayedRescale(zoomDelay));
 
-    root.on('resize', event => {
-        console.log('root resized')
-        size = root.node().getBoundingClientRect();
-        svg.attr('viewBox', [0, 0, size.width, size.height]);
-    });
-
-    let xScale = d3.scaleLinear()
-        .domain(plotRanges[0])
-        .range([0, size.width]);
-
-    let yScale = d3.scaleLinear()
-        .domain(plotRanges[1])
-        .range([size.height, 0]);
+    let xScale = d3.scaleLinear().domain(plotRanges[0]).range([0, size.width]);
+    let yScale = d3.scaleLinear().domain(plotRanges[1]).range([size.height, 0]);
 
     let xAxis = g => g.call(d3.axisBottom(xScale));
-
     let yAxis = g => g.call(d3.axisLeft(yScale));
 
     let line = d3.line()
@@ -49,7 +47,10 @@ export function plotter() {
         .x(d => xScale(d.x))
         .y(d => yScale(d.y));
 
-    svg.append('path')
+    let defTr = d3.transition().duration(200);
+
+    load();
+    let path = svg.append('path')
         .datum(data)
         .attr('class', 'plot-line')
         .attr('d', line)
@@ -66,17 +67,32 @@ export function plotter() {
         .attr('transform', `translate(${size.width / 2}, 0)`)
         .call(yAxis);
 
-    function rescale(factor) {
-        plotRanges = plotRanges.map(([min, max]) => [min * factor, max * factor]);
+    function resize() {
         xScale.domain(plotRanges[0]);
         yScale.domain(plotRanges[1]);
+        svg.select('.xaxis').transition().call(xAxis);
+        svg.select('.yaxis').transition().call(yAxis);
+        redraw();
+    }
 
-        svg.select('.xaxis')
-            .transition().duration(350)
-            .call(xAxis);
-        svg.select('.yaxis')
-            .transition().duration(350)
-            .call(yAxis);
+    function SVGResizeHandler(event) {
+        size = svg.node().getBoundingClientRect();
+        svg.attr('viewBox', [0, 0, size.width, size.height]);
+        resize();
+    }
+    
+    function redraw() {
+        load();
+        path.datum(data);
+        console.log(data);
+        console.log(plotRanges);
+        svg.select('.plot-line')
+            .attr('d', line);
+    }
+
+    function rescale(factor) {
+        plotRanges = plotRanges.map(([min, max]) => [min * factor, max * factor]);
+        resize();
     }
     // the rescaling is performed only after a set delay time has passed
     // without wheel events being triggered, and accumulating the
@@ -86,7 +102,9 @@ export function plotter() {
         let factorAcc = 1.0;
 
         return event => {
-            factorAcc += event.deltaY < 0 ? -0.1 : 0.1;
+            if (event.deltaY < 0) factorAcc /= zoomInterval;
+            else factorAcc *= zoomInterval;
+
             if (timeout) clearTimeout(timeout);
             timeout = setTimeout (() => {
                 rescale(factorAcc);
@@ -94,5 +112,4 @@ export function plotter() {
             }, delay);
         };
     }
-    svg.on('wheel', delayedRescale(200));
 }
